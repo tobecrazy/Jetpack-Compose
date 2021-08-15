d48482df308c201a861faa208242c8ba974a7c62
Young Liu <young.liu@sap.com>
2021-08-15 22:42:55 +0800

    implement list items

---
+++ app/src/main/java/com/young/pdfreader/list/Items.kt
@@ -0,0 +1,14 @@
+package com.young.pdfreader.list
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+/**
+ * Create by Young on 08/15/2021
+ **/
+data class Items(
+    val title: String,
+    val description: String,
+    val iconId: ImageVector?,
+    val contentDescription: String?,
+    val overLineText: String
+)
