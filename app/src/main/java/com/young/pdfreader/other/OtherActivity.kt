05cf3cdc6afe5fefa343e5ef6bed83362e560302
Young Liu <young.liu@sap.com>
2021-09-15 09:42:15 +0800

    add snackbar

--- app/src/main/java/com/young/pdfreader/other/OtherActivity.kt
+++ app/src/main/java/com/young/pdfreader/other/OtherActivity.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
 import com.young.pdfreader.R
 import com.young.pdfreader.data.COMMON_REQUEST_CODE
 import com.young.pdfreader.data.DOWNLOAD_REQUEST_CODE
+import com.young.pdfreader.data.TRANSLUCENT_REQUEST_CODE
 import com.young.pdfreader.data.UPLOAD_REQUEST_CODE
 import com.young.pdfreader.databinding.ActivityOtherBinding
 
@@ -19,15 +21,53 @@ class OtherActivity : AppCompatActivity(), View.OnClickListener {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_other)
         binding.showBottomSheetDialog.setOnClickListener(this)
         binding.requestAct.setOnClickListener(this)
+        binding.translucentBtn.setOnClickListener(this)
+        binding.snackbarBtn.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         Log.d("OtherActivity", "===>${view?.id}")
-        if (view?.id == binding.showBottomSheetDialog.id) {
-            CustomBottomSheetFragment.getInstance(this, null)
-        } else if (view?.id == binding.requestAct.id) {
-            val intent = Intent(this, RequestActivity::class.java)
-            startActivityForResult(intent, COMMON_REQUEST_CODE)
+        when (view?.id) {
+            binding.showBottomSheetDialog.id -> {
+                CustomBottomSheetFragment.getInstance(this, null)
+            }
+            binding.requestAct.id -> {
+                val intent = Intent(this, RequestActivity::class.java)
+                startActivityForResult(intent, COMMON_REQUEST_CODE)
+            }
+            binding.translucentBtn.id -> {
+                val intent = Intent(this, TranslucentActivity::class.java)
+                startActivityForResult(intent, TRANSLUCENT_REQUEST_CODE)
+            }
+            binding.snackbarBtn.id -> {
+                val bottomView = binding.snackbarBtn
+//                val bottomView = this.window.decorView.rootView
+                var snackbar = Snackbar.make(
+                    bottomView,
+                    "Show Toast,click to show bottom sheet dialog!",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.setAction("Show") {
+                    CustomBottomSheetFragment.getInstance(this, null)
+                }
+                val callback = object : Snackbar.Callback() {
+                    override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                        Log.d("OtherActivity", "===>$event")
+                        super.onDismissed(transientBottomBar, event)
+                    }
+
+                    override fun onShown(sb: Snackbar?) {
+                        Log.d("OtherActivity", "===>on show")
+                        super.onShown(sb)
+                    }
+                }
+
+                snackbar.addCallback(callback)
+                snackbar.show()
+            }
+            else -> {
+
+            }
         }
     }
 
