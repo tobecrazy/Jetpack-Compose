9e65ddffc7e89fb8299ab2572c68d55c0a931f32
Young Liu <young.liu@sap.com>
2022-04-23 22:02:22 +0800

    update gradle

--- app/src/main/java/com/young/pdfreader/other/DownloadActivity.kt
+++ app/src/main/java/com/young/pdfreader/other/DownloadActivity.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import com.young.pdfreader.R
 
 class DownloadActivity : AppCompatActivity() {
@@ -14,13 +15,19 @@ class DownloadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download)
         val downloadBtn = findViewById<AppCompatButton>(R.id.close_download_btn)
+        val textViewDesc = findViewById<AppCompatTextView>(R.id.tv_content_desc)
+        textViewDesc.setText(R.string.app_name)
+        textViewDesc.setTextAppearance(R.style.TextAppearance_AppCompat_Body1)
+        downloadBtn.setTextAppearance(R.style.TextAppearance_AppCompat_Button)
         downloadBtn.setOnClickListener {
+            textViewDesc.setTextAppearance(R.style.TextAppearance_AppCompat_Caption)
+            textViewDesc.setText(R.string.migrate_desc)
             Looper.myLooper()?.let {
                 Handler(it).postDelayed({
                     Log.d("OtherActivity", "===>finish  === downloadActivity")
                     this.setResult(Activity.RESULT_OK)
                     finish()
-                }, 2000)
+                }, 10000)
             }
         }
     }
