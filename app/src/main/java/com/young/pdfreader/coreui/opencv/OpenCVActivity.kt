c159b0bd7ec78f95d0cd3c6a82e11c37b33b65a8
Young Liu <young.liu@sap.com>
2024-05-18 12:57:32 +0800

    update code

--- app/src/main/java/com/young/pdfreader/coreui/opencv/OpenCVActivity.kt
+++ app/src/main/java/com/young/pdfreader/coreui/opencv/OpenCVActivity.kt
@@ -1,14 +1,29 @@
 package com.young.pdfreader.coreui.opencv
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.young.pdfreader.R
 import com.young.pdfreader.coreui.base.BaseActivity
 import com.young.pdfreader.databinding.ActivityOpenCvactivityBinding
 
-class OpenCVActivity : BaseActivity() {
+class OpenCVActivity : BaseActivity(), View.OnClickListener {
     private lateinit var binding: ActivityOpenCvactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpenCvactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imagesResize.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.images_resize) {
+            Toast.makeText(this, "Resize Image", Toast.LENGTH_SHORT).show()
+            val newParams = binding.imagesResize.layoutParams
+            newParams.width = resources.getDimensionPixelSize(R.dimen.common_200dp)
+            newParams.height = resources.getDimensionPixelSize(R.dimen.common_200dp)
+            binding.imagesResize.layoutParams = newParams
+        }
     }
 }
